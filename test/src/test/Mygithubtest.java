200de34e5d68de492c82b4e5e1e4777f0f8ceb06
admin <admin@qq.com>
2020-11-18 19:08:54 +0800

    更改提交

--- test/src/test/Mygithubtest.java
+++ test/src/test/Mygithubtest.java
@@ -1,6 +1,7 @@
 package test;
 
 public class Mygithubtest {
+	
 	public static void main(String[] args) {
 		System.out.println("这是我的测试！！！！");
 	}
@@ -11,6 +12,11 @@ public class Mygithubtest {
 	
 	public void delete() {
 		System.out.println("这是删除方法！！！");
+	}
+	
+	private void ad() {
+	System.out.println("11111");
+
 	}
 	
 }
